e34229eb846879903510aff9c9726e3ddc1810ed
Gerhard Coetzee <gerhard.coetzee@live.com>
2019-11-12 08:32:47 +0200

    Add files via upload

---
+++ terraform-vdc-infra-win64/main.tf
@@ -0,0 +1,34 @@
+provider "azurerm"{}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "SBSA-CIB-Risk-Infrastructure"
+    storage_account_name = "sbsasancibrisk001"
+    container_name       = "sbsasancibrisk001"
+    key                  = "starter.terraform.tfstate"
+  }
+}
+
+module "terraform_vdc_infra_win64" {
+  source = "./vdc-infra-managed"
+
+  hostname = "${var.hostname}"
+  deployment_rg = "${var.deployment_rg}"
+  deployment_location = "${var.deployment_location}"
+  network_security_group = "${var.network_security_group}"
+  virtual_network_name = "${var.virtual_network_name}"
+  address_space = "${var.address_space}"
+  subnet_id = "${var.subnet_id}"
+  vm_size = "${var.vm_size}"
+  image_publisher = "${var.image_publisher}"
+  image_offer = "${var.image_offer}"
+  image_sku = "${var.image_sku}"
+  image_version = "${var.image_version}"
+  admin_username = "${var.admin_username}"
+  admin_pw = "${var.admin_pw}"
+  diagnostics_storage = "${var.diagnostics_storage}"
+  diagnostics_resource_group = "${var.diagnostics_resource_group}"
+   # General Variables
+  tag_source_deployment = "${var.tag_source_deployment}"
+  tag_environment_deployment = "${var.tag_environment_deployment}"
+}
